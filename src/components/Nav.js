7e7a9ea91c70b6b4068cbf49841a7c42de6439f4
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-14 17:03:08 +0000

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+        <ul>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Sites</li>
+          <li>Your Data</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
