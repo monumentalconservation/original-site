78b6d8c6a481f0fa6d972784be5c6c35551a6085
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-22 15:46:07 +0000

    Updates

---
+++ precache-manifest.8abf5a3cd9ae97c26dae5f51a77296cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f880e61b1f5fc40b10c17266e82e70d",
+    "url": "/new-site/static/media/mmt-main-logo.6f880e61.png"
+  },
+  {
+    "revision": "3f921f367d62662b9686",
+    "url": "/new-site/static/css/main.7c0b6bd2.chunk.css"
+  },
+  {
+    "revision": "8007b0098f4c0491c3a8",
+    "url": "/new-site/static/js/runtime~main.19b5233a.js"
+  },
+  {
+    "revision": "72701de75eb0c766bcaa262f45aefafa",
+    "url": "/new-site/static/media/lochranza-rainbow.72701de7.jpg"
+  },
+  {
+    "revision": "dd1ecb5d024a6ca76331",
+    "url": "/new-site/static/js/2.33c17e85.chunk.js"
+  },
+  {
+    "revision": "3f921f367d62662b9686",
+    "url": "/new-site/static/js/main.b09d8c6c.chunk.js"
+  },
+  {
+    "revision": "1dcd36e083af33dfbac80fdbae4bb797",
+    "url": "/new-site/static/media/me-photo-min.1dcd36e0.jpg"
+  },
+  {
+    "revision": "2adde48eb89ff0702bc296edd18673d3",
+    "url": "/new-site/static/media/ness.2adde48e.jpg"
+  },
+  {
+    "revision": "acd8e116a57dc95c43ebf7a4caa8c586",
+    "url": "/new-site/static/media/loch-doon.acd8e116.jpg"
+  },
+  {
+    "revision": "dd1ecb5d024a6ca76331",
+    "url": "/new-site/static/css/2.d82b5b93.chunk.css"
+  },
+  {
+    "revision": "e66697dda4f17f795fef75005e26d1a3",
+    "url": "/new-site/index.html"
+  }
+];
