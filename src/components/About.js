1e872163cd862b18dedd5ce99d12c70615804512
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-25 16:57:06 +0000

    adds about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import ImageTile from './ImageTile'
+
+import headshot from '../images/headshot-portrait.jpg'
+import josep from '../images/josep.jpg'
+import lyn from '../images/lyn-taking-photo.jpg'
+
+
 
 class About extends Component {
   render() {
     return (
-      <div className="about">
-        Right...
+      <div className="about ph4 w-80-ns center flex flex-wrap">
+        <section className="w-100 w-50-ns mt4 mb4 mb4-ns pa4-ns">
+          <ImageTile 
+            title="Rosie Brigham, PhD Researcher"
+            image={headshot} 
+            text="Rosie studied History of Art and Conservation at UCL, though shortly after graduating she re-trained as a software engineer. After several years of working as a full stack and backend developer she re-joined academia to specialise in Citizen Heritage Science, and started the Monument Monitor project."
+            twitter="@rosie934"
+            email="rosie.brigham.10@ucl.ac.uk" />
+        </section>
+
+
+        <section className="w-100 w-50-ns mb4 mt6-ns mb4-ns pa4-ns">
+          <ImageTile 
+            title="Josep Grau-Bove, Academic Supervisor"
+            image={josep} 
+            text="Josep is a Heritage Scientist currently working at the Institute of Sustainable Heritage. He is the director of the the director for the MRes in Science in Egineering in Arts, Heritage and Archeology as well and the MSc in Heritage Data Science."
+            twitter="@lomecreu" />
+        </section>
+
+        <section className="w-100 w-50-ns mb4 mb4-ns pa4-ns">
+        <ImageTile 
+            title="Lyn Wilson, Heritage Supervisor"
+            image={lyn} 
+            text="Lyn has over 20 years’ experience in digital documentation practice and conservation/archaeological science. She is currently responsible for digital documentation and the application of cutting-edge technologies within Historic Environment Scotland and managed the Scottish Ten project on behalf of the Scottish Government"
+            twitter="@Scottish3D" />
+        </section>
       </div>
     );
   }
---
+++ src/components/ImageTile.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+// overlay for image tile
+const Overlay = ({showInfo, title, text}) => (
+  <div 
+    className="absolute w-100 h-100 flex items-center pa3 pa4-ns overlay"
+    style={{
+      transform: showInfo? 'none' : 'translateY(-100%)'
+    }}
+  >
+    <div>
+      <h1 className="f5 f3-ns mt0 mb2 regular lh-title">{title}</h1>
+      <p className="lh-copy-ns mv0 f6 f4-s measure-l">{text}</p>
+    </div>
+  </div>
+)
+
+class ImageTile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false
+    };
+    // explicitly bind this to made our function
+    this.toggleInfo = this.toggleInfo.bind(this);
+    this.closeInfo = this.closeInfo.bind(this);
+  }
+
+  toggleInfo() {
+    this.setState((prevState, props) => ({
+      showInfo: !prevState.showInfo
+    }));
+  }
+
+  closeInfo() {
+    this.setState({
+      showInfo: false
+    })
+  }
+
+  render () {
+    const {title, text, image, className} = this.props
+    const {showInfo} = this.state
+    return (
+      <div 
+        className={`ph0-l w-100 overflow-hidden pointer attraction ${className}`} 
+        onClick={this.toggleInfo}
+        // onMouseLeave={this.closeInfo}
+      >
+        <div className="relative">
+          <Overlay {...this.props}{...this.state}/>
+          <img src={`${image}`} className="db grayscale" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ImageTile;
