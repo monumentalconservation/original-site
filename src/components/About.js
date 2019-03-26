081f6bc9309a92172039c4bc2c9b61f769e1cd3e
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-26 13:21:45 +0000

    improves about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,15 @@ import ImageTile from './ImageTile'
 
 import headshot from '../images/headshot-portrait.jpg'
 import josep from '../images/josep.jpg'
-import lyn from '../images/lyn-taking-photo.jpg'
-
-
+import lyn from '../images/lyn-bridge.jpg'
+import matija from '../images/matija.jpeg'
+import lewis from '../images/lewis.png'
 
 class About extends Component {
   render() {
     return (
-      <div className="about ph4 w-80-ns center flex flex-wrap">
-        <section className="w-100 w-50-ns mt4 mb4 mb4-ns pa4-ns">
+      <div className="about ph4 w-80-ns mb6 center flex flex-wrap">
+        <section className="tile w-100 w-50-ns mt4 mt0-ns mb4 mb0-ns ph4-ns">
           <ImageTile 
             title="Rosie Brigham, PhD Researcher"
             image={headshot} 
@@ -21,7 +21,7 @@ class About extends Component {
         </section>
 
 
-        <section className="w-100 w-50-ns mb4 mt6-ns mb4-ns pa4-ns">
+        <section className="tile w-100 w-50-ns mb4 mb0-ns ph4-ns pt5-ns">
           <ImageTile 
             title="Josep Grau-Bove, Academic Supervisor"
             image={josep} 
@@ -29,12 +29,28 @@ class About extends Component {
             twitter="@lomecreu" />
         </section>
 
-        <section className="w-100 w-50-ns mb4 mb4-ns pa4-ns">
-        <ImageTile 
+        <section className="tile w-100 w-50-ns mb4 mb0-ns ph4-ns">
+          <ImageTile 
             title="Lyn Wilson, Heritage Supervisor"
             image={lyn} 
-            text="Lyn has over 20 years’ experience in digital documentation practice and conservation/archaeological science. She is currently responsible for digital documentation and the application of cutting-edge technologies within Historic Environment Scotland and managed the Scottish Ten project on behalf of the Scottish Government"
+            text="Lyn Wilson is a heritage scientist with over 20 years’ experience in digital documentation practice and conservation/archaeological science. She has a BSc in Archaeology, MA and PhD in Archaeological Science. As Digital Documentation Manager at Historic Environment Scotland, Lyn is responsible for 3D recording the properties and collections in the care of HES for conservation, asset management, interpretation, education and to improve accessibility."
             twitter="@Scottish3D" />
+        </section>
+
+        <section className="w-100 w-50-ns mb4 mb0-ns ph4-ns pt5-ns">
+          <ImageTile 
+            title="Matija Strlic"
+            image={matija} 
+            text="Matija is Deputy Director of UCL Institute for Sustainable Heritage and Deputy Director of the EPSRC Centre for Doctoral Training in Arts, Heritage and Archaeology. Matija's research focus is in the cross-disciplinary field of heritage science, particularly development of new scientific tools and methods to study heritage materials and collections, and their interactions with the environment."
+            twitter="@matijastrlic" />
+        </section>
+
+        <section className="tile w-100 w-50-ns mb4 mb0-ns ph4-ns">
+          <ImageTile 
+              title="Lewis Griffin"
+              image={lewis} 
+              text="Lewis D Griffin received a BA degree in Mathematics & Philosophy from Oxford University, UK, in 1988, and a PhD degree from the University of London in 1995 for the thesis “Descriptions of Image Structure” in the area of computational vision. Following positions at Aston University (Vision Sciences) and Kings College London (Imaging Sciences) he has been at University College London (Computer Science) since 2005, where is now a Reader. His research interests include image structure, colour vision, machine learning and biomedical modelling, with applications in security science, biomedicine and geoscience."
+          />
         </section>
       </div>
     );
