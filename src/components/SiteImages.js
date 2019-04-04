bda1c893a0a226bcb2b3593d63a07c39df92a7db
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-04 15:05:59 +0100

    has all the images in one file

---
+++ src/components/SiteImages.js
@@ -0,0 +1,101 @@
+import caerlvaerock from '../images/sites/caerlvaerock.jpg'
+import drumtoddanStones from '../images/sites/drumtroddan-stones.jpg'
+import drumtroddanCup from '../images/sites/drumtroddan-cup.jpg'
+import lochDoon from '../images/sites/loch-doon.jpg'
+import bothwell from '../images/sites/bothwell.jpg'
+import castleSemple from '../images/sites/castle-semple.jpg'
+import rothesay from '../images/sites/rothesay.jpg'
+import stBlane from '../images/sites/st-blane.jpg'
+import lochranza from '../images/sites/lochranza.jpg'
+import machrie from '../images/sites/machrie.jpg'
+import achnabreck from '../images/sites/achnabreck2.jpg'
+import baluachraig from '../images/sites/baluachraig.jpg'
+import clava from '../images/sites/clava.jpg'
+import ness from '../images/sites/ness.jpg'
+import tarves from '../images/sites/tarves.jpg'
+import maidenStone from '../images/sites/maiden-stone.jpg'
+import tealing from '../images/sites/tealing.jpg'
+import kinneil from '../images/sites/kinneil.jpg'
+import tantallon from '../images/sites/tantallon.jpg'
+
+const Images = [
+  {
+    image: caerlvaerock,
+    name: "Caerlvaerock"
+  },
+  {
+    image: drumtoddanStones,
+    name: "Drumtroddan Standing Stones"
+  },
+  {
+    image: drumtroddanCup,
+    name: "Drumtroddan Cup and Ring Marks"
+  },
+  {
+    image: lochDoon,
+    name: "Loch Doon"
+  },
+  {
+    image: bothwell,
+    name: "Bothwell Castle"
+  },
+  {
+    image: castleSemple,
+    name: "Castle Semple Colligiate Church"
+  },
+  {
+    image: rothesay,
+    name: "Rothesay Castle"
+  },
+  {
+    image: stBlane,
+    name: "St Blane's Church"
+  },
+  {
+    image: lochranza,
+    name: "Lochranza Castle"
+  },
+  {
+    image: machrie,
+    name: "Machrie Moor Standing Stones"
+  },
+  {
+    image: achnabreck,
+    name: "Achnabreck Cup and Ring Marks"
+  },
+  {
+    image: baluachraig,
+    name: "Baluachriag Cup and Ring Marks"
+  },
+  {
+    image: clava,
+    name: "Clava Cairns"
+  },
+  {
+    image: ness,
+    name: "Ness of Burgi"
+  },
+  {
+    image: tarves,
+    name: "Tarves Tomb"
+  },
+  {
+    image: maidenStone,
+    name: "Maiden Stone"
+  },
+  {
+    image: tealing,
+    name: "Tealing Soutterain"
+  },
+  {
+    image: kinneil,
+    name: "Kinneil House"
+  },
+  {
+    image: tantallon,
+    name: "Tantallon Castle"
+  }
+]
+
+
+export default Images;
