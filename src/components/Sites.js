ad43b12e7d6dee0104897c4aa14b41625d89bb9c
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-04 15:18:53 +0100

    makes it a bit responsive

--- src/components/Sites.js
+++ src/components/Sites.js
@@ -6,14 +6,36 @@ import SiteImages from './SiteImages'
 import SiteImage from './SiteImage'
 
 
-
+// TODO - make this responsive...
 class Sites extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      columnCount: this.calculateColumnCount
+    }
+
+    window.addEventListener("resize", this.updateColumnCount)
+  }
+
+  calculateColumnCount = () => {
+    return (window.innerWidth < 1100 ? 1 : 3)
+  }
+
+  updateColumnCount = () => {
+    this.setState({
+      columnCount: this.calculateColumnCount
+    })
+  }
+
   render() {
+    // const width = window.innerWidth
+    const columnCount = (window.innerWidth < 1100 ? 1 : 3)
     return (
       <div className="sites">
         <Border alignment="left" />
         <div className="about ph4 ph0-m mb6 center">
-          <MasonryLayout columns={3} gap={25}>
+          <MasonryLayout columns={columnCount} gap={25}>
           {
             SiteImages.map(item => {
               return (
