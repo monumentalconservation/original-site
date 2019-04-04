6d724b2eccccbe1cc3472d5359fec0df7dd7a281
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-04 15:06:39 +0100

    Creates sites page

---
+++ src/components/SiteImage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SiteImage extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {image, name} = this.props
+    return (
+      <div className="site">
+        <img className="grayscale" src={image}/>
+        <div className="site-name">
+          <h3 className="f3 ma0">{name}</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SiteImage;
--- src/components/Sites.js
+++ src/components/Sites.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Border from './Border'
+import ImageTile from './ImageTile'
+import MasonryLayout from './MasonryLayout'
+import SiteImages from './SiteImages'
+import SiteImage from './SiteImage'
+
+
 
 class Sites extends Component {
   render() {
     return (
-      <div>
-        WHY IS THIS NOT WORKING?!
+      <div className="sites">
+        <Border alignment="left" />
+        <div className="about ph4 ph0-m mb6 center">
+          <MasonryLayout columns={3} gap={25}>
+          {
+            SiteImages.map(item => {
+              return (
+                <SiteImage {...item} />
+              )
+            })
+          }
+          </MasonryLayout>
+          
+        </div>
       </div>
     );
   }
