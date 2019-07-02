8057e4a97f7c393a79b7b7ff9224eed6d83f0459
Rosie Brigham <rosiebrigham@gmail.com>
2019-07-02 21:18:49 +0100

    copy files over from reformatted machine

---
+++ src/components/data/ResultsAndPublications.js
@@ -0,0 +1,30 @@
+const resultsAndPublications = [
+  {
+    title: 'Introducing Monument Monitor - Historic Environment Scotland Blog',
+    link: 'https://blog.historicenvironment.scot/2019/05/monument-monitor/',
+    description: 'Historic Environment Scotland takes up the pen to publicise this project, learn how you can be a ‘citizen scientist’ at 20 of Scotland’s most evocative historic sites as part of an exciting new conservation project.',
+    imageLink: 'https://blog.historicenvironment.scot/wp-content/uploads/2019/05/machrie-moor-twitter-submission.png',
+    styles: ''
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    imageLink: '',
+    styles: ''
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    imageLink: '',
+    styles: ''
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    imageLink: '',
+    styles: ''
+  }
+];
